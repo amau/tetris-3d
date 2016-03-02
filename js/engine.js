780e1f9e871e374f2a0ae113614920cfc0bf2290
amaurs <amaury.gtz@gmail.com>
2016-03-02 10:18:14 -0600

    Refactoring and improvement of documentation.

--- js/engine.js
+++ js/engine.js
@@ -1,231 +1,3 @@
-var board;
-
-var interval;
-
-// Representation of tetris pieces with shape and color.
-var i = { id: 'i', blocks: [0x0F00, 0x2222, 0x00F0, 0x4444], colorName: 'cyan'   , color: 1};
-/*
-
-0000
-1111
-0000
-0000
-
-0010
-0010
-0010
-0010
-
-0000
-0000
-1111
-0000
-
-0100
-0100
-0100
-0100
-*/
-var j = { id: 'j', blocks: [0x44C0, 0x8E00, 0x6440, 0x0E20], colorName: 'blue'   , color: 2};
-/*
-0100
-0100
-1100
-0000
-
-1000
-1110
-0000
-0000
-
-0110
-0100
-0100
-0000
-
-0000
-1110
-0010
-0000
-*/
-var l = { id: 'l', blocks: [0x4460, 0x0E80, 0xC440, 0x2E00], colorName: 'orange' , color: 3};
-/*
-0100
-0100
-0110
-0000
-
-0000
-1110
-1000
-0000
-
-1100
-0100
-0100
-0000
-
-0010
-1110
-0000
-0000
-*/
-var o = { id: 'o', blocks: [0xCC00, 0xCC00, 0xCC00, 0xCC00], colorName: 'yellow' , color: 4};
-/*
-1100
-1100
-0000
-0000
-
-1100
-1100
-0000
-0000
-
-1100
-1100
-0000
-0000
-
-1100
-1100
-0000
-0000
-*/
-var s = { id: 's', blocks: [0x06C0, 0x8C40, 0x6C00, 0x4620], colorName: 'green'  , color: 5};
-
-/*
-0000
-0110
-1100
-0000
-
-1000
-1100
-0100
-0000
-
-0110
-1100
-0000
-0000
-
-0100
-0110
-0010
-0000
-*/
-var t = { id: 't', blocks: [0x0E40, 0x4C40, 0x4E00, 0x4640], colorName: 'purple' , color: 6};
-/*
-0000
-1110
-0100
-0000
-
-0100
-1100
-0100
-0000
-
-0100
-1110
-0000
-0000
-
-0100
-0110
-0100
-0000
-*/
-var z = { id: 'z', blocks: [0x0C60, 0x4C80, 0xC600, 0x2640], colorName: 'red'    , color: 7};
-/*
-0000
-1100
-0110
-0000
-
-0100
-1100
-1000
-0000
-
-1100
-0110
-0000
-0000
-
-0010
-0110
-0100
-0000
-*/
-
-var COLORS = 
-{
- 0:'white',
- 1:'cyan', 
- 2:'blue', 
- 3:'orange', 
- 4:'yellow', 
- 5:'green', 
- 6:'purple', 
- 7:'red'
-}
-
-// Size of our board.
-var COLUMNS = 10;
-var ROWS = 22;
-var TETRIMINO_SIZE = 4;
-var TETRIMINO_HEIGHT = TETRIMINO_SIZE;
-var TETRIMINO_WIDTH = TETRIMINO_SIZE;
-
-
-
-
-/**
-This method will remove the n-th line by moving the lines below up.
-**/
-function removeLine(n)
-{
-    /**
-    var y;
-    for(y = n; y > 0; y--)
-    {
-        board[y] = board[y - 1];
-    }
-    board[0] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-    **/
-}
-function keyPress( key ) {
-    switch ( key ) {
-        case 'left':
-            if(isValidMove(current, horizontal - 1, vertical, rotation, board))
-            {
-                horizontal--;
-            }
-            break;
-        case 'right':
-            if(isValidMove(current, horizontal + 1, vertical, rotation, board))
-            {
-               horizontal++;
-            }
-            break;
-        case 'down':
-            if(isValidMove(current, horizontal, vertical + 1, rotation, board))
-            {
-                vertical++;
-            }
-            break;
-        case 'rotate':
-	    rot = (rotation+1) % 4;
-            if(isValidMove(current, horizontal, vertical, rot, board))
-            {
-                rotation = rot;
-            }
-            break;
-    }
-}
-
 /**
 This function writes the given array to the console.
 **/
@@ -242,6 +14,7 @@ function printBoardToConsole(board)
         row = "";
     }
 }
+
 /**
 This function renders the given in a html tbody element.
 **/
@@ -264,6 +37,7 @@ function printBoardToElement(board, element)
         tr.appendChild(td);
     }
 }
+
 /**
 Helper function that encapsulates the retrieval of the color from the color dictionary.
 **/
@@ -271,6 +45,7 @@ function getColor(index)
 {
     return COLORS[index];
 }
+
 /**
 This function will render with a tetrimino in the size of the board. For example:
 renderTetrimino(2, z, 2)
@@ -385,6 +160,7 @@ function drawTetriminoOnBoard(rowNum, renderedTetrimino, thisBoard)
     }
     return arr;
 }
+
 /**
 Creates a copy of the board and returns the new array.
 **/
@@ -400,6 +176,7 @@ function copyBoard(board)
     }
     return arr;
 }
+
 /**
 Creates a zero filled array with the dimensions of regular tetris board.
 **/
@@ -407,6 +184,7 @@ function createCleanBoard()
 {
     return createZerosGrid(COLUMNS, ROWS);
 }
+
 /**
 Creates a zero filled array with given dimensions.
 **/
@@ -425,6 +203,7 @@ function createZerosGrid(width, height)
     }
     return arr;
 }
+
 /**
 If the position is feasible, this method returns a new array with the
 configuration given.
@@ -450,16 +229,10 @@ function isValidMove(tetrimino, x, y, rotation, board)
     }
 }
 
-var vertical = 0;
-var horizontal = 4;
-var rotation = 0;
-var current;
-var pieces = [i,s,z,l,j,o,t];
-
 /**
 This function will check if moving the current piece one position down.
 **/
-function newUpdate()
+function update()
 {
     
     newBoard = isValidMove(current, horizontal, vertical + 1 , rotation, board);
@@ -495,13 +268,12 @@ function newUpdate()
 }
 
 /**
-
+Looks for complete lines in the board, if it finds one, the line will be erased from the board. It returns a clone of the board without the found lines.
 **/
 function checkLines(board)
 {
     var arr = createCleanBoard();
     var counter = board.length - 1;
-
     for(var k = board.length - 1; k > -1; k--)
     {
         rowSum = 0;
@@ -532,11 +304,12 @@ function newPiece()
     current = pieces[Math.floor((Math.random() * pieces.length))];    
 }
 
-
+/**
+This method initializes the board to a clean one. This method must be called before the game loop starts.
+**/
 function init()
 {
     board = createCleanBoard();
-
 }
 
 /**
@@ -547,28 +320,138 @@ function tetris()
     clearInterval(interval);
     init();
     newPiece();
-    interval = setInterval(newUpdate, 200);    
+    interval = setInterval(update, 200);    
 }
 
-
-
-
-document.addEventListener("keydown", keyDownTextField, false);
-
-function keyDownTextField( e ) 
+/**
+Helper function that maps key codes to actions.
+**/
+function keyDownTextField(event) 
 {
-    console.log("Key pressed");
     var keys = {
         37: 'left',
         39: 'right',
         40: 'down',
         38: 'rotate'
     };
-    if ( typeof keys[ e.keyCode ] != 'undefined' ) 
+    if(typeof keys[event.keyCode] != 'undefined')
     {
-        keyPress( keys[ e.keyCode ] );
-        //update();
+        keyPress(keys[event.keyCode]);
     }
 }
 
+/**
+Handles the actions when a key is pressed. We only have four valid moves.
+**/
+function keyPress( key ) {
+    switch ( key ) {
+        case 'left':
+            if(isValidMove(current, horizontal - 1, vertical, rotation, board))
+            {
+                horizontal--;
+            }
+            break;
+        case 'right':
+            if(isValidMove(current, horizontal + 1, vertical, rotation, board))
+            {
+               horizontal++;
+            }
+            break;
+        case 'down':
+            if(isValidMove(current, horizontal, vertical + 1, rotation, board))
+            {
+                vertical++;
+            }
+            break;
+        case 'rotate':
+	    rot = (rotation + 1) % 4;
+            if(isValidMove(current, horizontal, vertical, rot, board))
+            {
+                rotation = rot;
+            }
+            break;
+    }
+}
+
+var board;
+
+var interval;
+
+// Representation of tetris pieces with shape and color.
+var I = { id: 'i', blocks: [0x0F00, 0x2222, 0x00F0, 0x4444], colorName: 'cyan'   , color: 1};
+/*
+
+0000 0010 0000 0100
+1111 0010 0000 0100
+0000 0010 1111 0100
+0000 0010 0000 0100
+*/
+var J = { id: 'j', blocks: [0x44C0, 0x8E00, 0x6440, 0x0E20], colorName: 'blue'   , color: 2};
+/*
+0100 1000 0110 0000
+0100 1110 0100 1110
+1100 0000 0100 0010
+0000 0000 0000 0000
+*/
+var L = { id: 'l', blocks: [0x4460, 0x0E80, 0xC440, 0x2E00], colorName: 'orange' , color: 3};
+/*
+0100 0000 1100 0010
+0100 1110 0100 1110
+0110 1000 0100 0000
+0000 0000 0000 0000
+*/
+var O = { id: 'o', blocks: [0xCC00, 0xCC00, 0xCC00, 0xCC00], colorName: 'yellow' , color: 4};
+/*
+1100 1100 1100 1100
+1100 1100 1100 1100
+0000 0000 0000 0000
+0000 0000 0000 0000
+*/
+var S = { id: 's', blocks: [0x06C0, 0x8C40, 0x6C00, 0x4620], colorName: 'green'  , color: 5};
+/*
+0000 1000 0110 0100
+0110 1100 1100 0110
+1100 0100 0000 0010
+0000 0000 0000 0000
+*/
+var T = { id: 't', blocks: [0x0E40, 0x4C40, 0x4E00, 0x4640], colorName: 'purple' , color: 6};
+/*
+0000 0100 0100 0100
+1110 1100 1110 0110
+0100 0100 0000 0100
+0000 0000 0000 0000
+*/
+var Z = { id: 'z', blocks: [0x0C60, 0x4C80, 0xC600, 0x2640], colorName: 'red'    , color: 7};
+/*
+0000 0100 1100 0010
+1100 1100 0110 0110
+0110 1000 0000 0100
+0000 0000 0000 0000
+*/
+
+var COLORS = 
+{
+   0:'white',
+   1:'cyan', 
+   2:'blue', 
+   3:'orange', 
+   4:'yellow', 
+   5:'green', 
+   6:'purple', 
+   7:'red'
+}
+
+// Constants that will be used along the game.
+var COLUMNS = 10;
+var ROWS = 22;
+var TETRIMINO_SIZE = 4;
+var TETRIMINO_HEIGHT = TETRIMINO_SIZE;
+var TETRIMINO_WIDTH = TETRIMINO_SIZE;
+
+var vertical = 0;
+var horizontal = 4;
+var rotation = 0;
+var current;
+var pieces = [I,S,Z,L,J,O,T];
+document.addEventListener("keydown", keyDownTextField, false);
 tetris();
